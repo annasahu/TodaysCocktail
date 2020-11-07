520cec8181337a400ed7242d251b8d69efada1f6
Anna Sahu <73133104+annasahu@users.noreply.github.com>
2020-11-06 21:45:58 -0300

    adding repository

--- app/src/main/java/com/wcc/todayscocktail/network/CocktailsApiService.kt
+++ app/src/main/java/com/wcc/todayscocktail/network/CocktailsApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
-private const val BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1"
+private const val BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/"
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
---
+++ app/src/main/java/com/wcc/todayscocktail/repository/CocktailsListRepository.kt
@@ -0,0 +1,41 @@
+package com.wcc.todayscocktail.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.wcc.todayscocktail.network.Cocktail
+import com.wcc.todayscocktail.network.CocktailsApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class CocktailsListRepository() {
+    //holder de dados interno da classe
+    private val cocktailListResponse = MutableLiveData<List<Cocktail>>()
+
+    //expoe os dados retornados pelo serviço
+    val  cocktailList: LiveData<List<Cocktail>>
+        get() = cocktailListResponse
+
+    //executado quando a classe é criada
+    init {
+        getCocktailsList()
+    }
+
+    //requisição dos dados dos coqueteis através do retrofit
+    private fun getCocktailsList() {
+        //executando tarefas fora da main thread
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val listResult = CocktailsApi.retrofitService.getNonAlcoholicCocktails().cocktailsList
+                cocktailListResponse.postValue(listResult)
+
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    cocktailListResponse.postValue(listOf())
+                }
+            }
+        }
+    }
+}
+
